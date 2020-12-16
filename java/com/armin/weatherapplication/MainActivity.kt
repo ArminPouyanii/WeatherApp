bebb604e2cba27dbce79af613d151d869abe7898
ArminPouyanii <armin.pouyani28@gmail.com>
2020-12-16 22:20:50 +0330

    set retrofit to main activity

--- java/com/armin/weatherapplication/MainActivity.kt
+++ java/com/armin/weatherapplication/MainActivity.kt
@@ -1,39 +1,34 @@
 package com.armin.weatherapplication
 
-import android.annotation.SuppressLint
-import android.content.Context
-import android.content.pm.PackageManager
-import android.location.Address
-import android.location.Geocoder
-import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Looper
 import android.util.Log
 import android.widget.Button
 import android.widget.TextView
-import android.widget.Toast
-import androidx.core.app.ActivityCompat
-import com.armin.weatherapplication.utils.RetrofitConnection
-import com.armin.weatherapplication.utils.ServiceLocation
-import com.google.android.gms.location.*
-import java.util.*
+import com.armin.weatherapplication.controller.ControllerWeather
+import com.armin.weatherapplication.controller.RetrofitConnection
+import com.armin.weatherapplication.view.IShowData
+
+const val BASE_URL="https://api.openweathermap.org/"
+const val lat="47.06"
+const val lon="34.31"
+const val apiId="10e6644f562878ec1f3babe9478ec058"
+
+class MainActivity : AppCompatActivity(),IShowData {
 
 
-class MainActivity : AppCompatActivity() {
     private var TAG = "mainActivity"
     private val PERMISSION_ID = 1000
-    private var response = RetrofitConnection()
     private var txt: TextView? = null
     private var txtLon: TextView? = null
     private var txtLat: TextView? = null
     private var txtCountry: TextView? = null
-
+    private lateinit var responce:RetrofitConnection
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        responce= RetrofitConnection(this)
 
         val btn = findViewById<Button>(R.id.btn_check)
         txt = findViewById(R.id.txtView)
@@ -41,11 +36,21 @@ class MainActivity : AppCompatActivity() {
         txtLon = findViewById(R.id.txtLon)
         txtCountry = findViewById(R.id.txtAdress)
         btn.setOnClickListener {
-            response.serverData()
+            responce.onWeatherData(BASE_URL, lat, lon, apiId)
         }
 
     }
 
+    override fun onShowWeather(
+        country: String,
+        temp: String,
+        temp_max: String,
+        temp_min: String,
+        humidity: String,
+        pressure: String
+    ) {
+        Log.e("testData","$country -- $temp --$temp_max -- $temp_min -- $humidity -- $pressure ")
+    }
 
 }
 

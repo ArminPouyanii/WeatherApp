5dd2e9786e45d686d77f71b0b459c6efe8167d1c
ArminPouyanii <armin.pouyani28@gmail.com>
2020-12-16 22:42:57 +0330

    mvc

--- java/com/armin/weatherapplication/utils/InterfaceLocation.kt
+++
@@ -1,8 +0,0 @@
-package com.armin.weatherapplication.utils
-
-interface InterfaceLocation {
-    //create fun check enable/disable Location
-    fun isLocationEnable():Boolean
-
-    fun getNewLocation()
-}

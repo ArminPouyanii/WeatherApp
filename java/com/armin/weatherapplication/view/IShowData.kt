0e4507fd2daa59effc7a42a93b370e972d8b636d
ArminPouyanii <armin.pouyani28@gmail.com>
2020-12-16 22:22:22 +0330

    intrface with show data

--- java/com/armin/weatherapplication/view/IShowData.kt
+++ java/com/armin/weatherapplication/view/IShowData.kt
@@ -1,5 +1,5 @@
 package com.armin.weatherapplication.view
 
 interface IShowData {
-    fun showData(message:String)
+    fun onShowWeather(country: String,temp: String,temp_max: String,temp_min: String,humidity: String,pressure: String)
 }
